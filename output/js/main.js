c1d22074aa7a2fdaf89c320eefc3d55aff589c7e
guspowell <augustuspowell@me.com>
2015-06-30 11:58:40 +0100

    added extra article and styled for iphone

--- output/js/main.js
+++ output/js/main.js
@@ -3,8 +3,20 @@ function setTitleContainerHeight() {
   $('#title-container').css("height", windowHeight);
 };
 
+function setOtherArticleArrowHeight() {
+  var leftTextHeight = $("#bottom-navigation .previous-article-text").height();
+  var rightTextHeight = $("#bottom-navigation .next-article-text").height();
+  console.log(leftTextHeight);
+  if( leftTextHeight > rightTextHeight ) {
+    $("#bottom-navigation.container img.arrow").css("height", leftTextHeight);
+  } else {
+    $("#bottom-navigation.container img.arrow").css("height", rightTextHeight);
+  }
+};
+
 $(document).ready(function() {
   setTitleContainerHeight();
+  setOtherArticleArrowHeight();
 });
 
 $(window).resize(function() {
