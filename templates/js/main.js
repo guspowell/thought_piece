04e380d77ff790079c1e005588016abdfb88a3f1
guspowell <augustuspowell@me.com>
2015-06-30 16:26:09 +0100

    finished?

--- templates/js/main.js
+++ templates/js/main.js
@@ -14,9 +14,22 @@ function setOtherArticleArrowHeight() {
   }
 };
 
+function verticallyAlignOverlayText() {
+  $("#bottom-navigation .article").each(function() {
+    var containerHeight = $(this).height();
+    var textHeight = $(this).find(".article-information-mobile").height();
+    var padding = 20;
+    var topMargin = (containerHeight/2) - (textHeight/2) - padding;
+    console.log(textHeight);
+    console.log(containerHeight);
+    $(this).find(".article-information-mobile").css("top", topMargin);
+  });
+}
+
 $(document).ready(function() {
   setTitleContainerHeight();
   setOtherArticleArrowHeight();
+  verticallyAlignOverlayText();
 });
 
 $(window).resize(function() {
